f05217ff125399dacdacdf127b427d4ed692c69c
unknown <pke7709@gmail.com>
2026-07-20 15:31:36 +0900

    update SEO sitemap2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,12 +11,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/app`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "yearly",
